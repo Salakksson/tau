795947a49ccf2b43a7c150c5b0798403c44b5624
Alex Salek <alexsalek2007@gmail.com>
2025-04-30 23:31:04 +0100

    add sensible configuration

--- pamde.sh
+++ pamde.sh
@@ -4,7 +4,7 @@ set -e
 
 print_help()
 {
-	echo "Usage: $0 [options]"
+	echo "Usage: $0 [options] (file)"
 	echo "Options:"
 	echo "  -r       Remove packages"
 	echo "  -a       Add packages"
@@ -14,12 +14,26 @@ print_help()
 	echo "  -q       Quiet mode"
 	echo "  -u       Use AUR"
 	echo "  -h       Display this help message"
+	echo "(file) defaults to /usr/local/share/pamde/main.conf"
 }
 
 if [ $# -eq 0 ]; then
-    print_help
-    exit 0
+	print_help
+	exit 0
 fi
+
+RESET='\033[0m'
+
+BOLD='\033[1m'
+
+FG_RED='\033[31m'
+FG_GREEN='\033[32m'
+FG_YELLOW='\033[33m'
+FG_BLUE='\033[34m'
+FG_MAGENTA='\033[35m'
+FG_CYAN='\033[36m'
+FG_WHITE='\033[97m'
+FG_GRAY='\033[90m'
 
 f_remove=false
 f_add=false
@@ -28,35 +42,84 @@ f_common=false
 f_force=false
 f_verbose=true
 f_aur=false
+SOURCE="/usr/local/share/pamde/main.conf"
+AUR_CMD="yay"
 
-while getopts "radcfquh" opt; do
-	case $opt in
-		r) f_remove=true ;;
-		a) f_add=true ;;
-		d) f_diff=true ;;
-		c) f_common=true ;;
-		f) f_force=true ;;
-		q) f_verbose=false ;;
-		u) f_aur=true ;;
-		h)
-			print_help
-			exit 0
-			;;
-		*)
+file_set=false
+while [ "$#" -gt 0 ]
+do
+	arg="$1"
+	case $arg in
+	-*)
+		flags=$(sed 's/-//' <<< "$arg")
+		for c in $(fold -w1 <<< "$flags")
+		do
+			case "$c" in
+			r) f_remove=true ;;
+			a) f_add=true ;;
+			d) f_diff=true ;;
+			c) f_common=true ;;
+			f) f_force=true ;;
+			q) f_verbose=false ;;
+			u) f_aur=true ;;
+			h)
+				print_help
+				exit 0
+				;;
+			*)
+				echo "Unknown flag -$c"
+				print_help
+				exit 1
+				;;
+			esac
+		done
+		;;
+	*)
+		if $file_set;
+		then
+			echo "Unexpected argument: $arg"
 			print_help
 			exit 1
-			;;
+		fi
+		SOURCE="$arg"
+		file_set=true
+		;;
 	esac
+	shift
 done
 
-AUR_CMD="yay"
+verbose()
+{
+	if $f_verbose;
+	then
+		echo -e $@
+	fi
+}
+
+if ! $file_set;
+then
+	verbose "Using default source '$SOURCE'"
+fi
+
+if ! test -e $SOURCE;
+then
+	echo -e "${BOLD}${FG_YELLOW}File '$SOURCE' does not exist"
+	echo -e "Create it and fill with current packages? (y/N) $RESET"
+	read responce
+	case $responce in
+		[y/Y]*)
+			sudo mkdir -p $(dirname "$SOURCE")
+			sudo chmod 777 $(dirname "$SOURCE")
+			touch "$SOURCE"
+			pacman -Qqe > "$SOURCE" ;;
+		*) return 1 ;;
+	esac
+fi
 
 if $f_aur;
 then
-	echo using $AUR_CMD for AUR
+	verbose using $AUR_CMD for AUR
 fi
-
-SOURCE="packages.conf"
 
 SOURCE_LIST=""
 SYSTEM_LIST=""
@@ -76,7 +139,10 @@ check_directive() # line
 }
 
 parse_file()
-{	file="$1"
+{
+	file="$1"
+	old_dir=$(pwd)
+	cd $(dirname $file)
 	while IFS= read -r line || test -n "$line";
 	do
 		line=$(sed 's/#.*//' <<< "$line")  # remove comments
@@ -91,8 +157,9 @@ parse_file()
 		then
 			SOURCE_LIST="$SOURCE_LIST $line"
 		fi
-				
+
 	done < "$file"
+	cd $old_dir # not using cd - since !incldues can be nested
 }
 
 query_packages()
@@ -140,27 +207,6 @@ do
 	fi
 done
 
-RESET='\033[0m'
-
-BOLD='\033[1m'
-
-FG_RED='\033[31m'
-FG_GREEN='\033[32m'
-FG_YELLOW='\033[33m'
-FG_BLUE='\033[34m'
-FG_MAGENTA='\033[35m'
-FG_CYAN='\033[36m'
-FG_WHITE='\033[97m'
-FG_GRAY='\033[90m'
-
-verbose()
-{
-	if $f_verbose;
-	then
-		echo -e $@
-	fi
-}
-
 confirm() # command
 {
 	if $f_force;
@@ -168,7 +214,7 @@ confirm() # command
 		$@
 		return 0
 	fi
-	
+
 	echo -e "$BOLD Are you sure you want to run: (y/N) $RESET"
 	echo -e "$FG_BLUE $@ $RESET"
 	read responce
@@ -189,7 +235,7 @@ add_packages()
 	then
 		confirm yay -S $PKGS_ADD
 	else
-		confirm sudo pacman -S $PKGS_ADD		
+		confirm sudo pacman -S $PKGS_ADD
 	fi
 }
 
@@ -209,7 +255,7 @@ print_diff()
 	then
 		verbose Common:
 		printf "${BOLD}${FG_BLUE}%s${RESET}\n" "$PKGS_COM"
-	fi	
+	fi
 }
 
 print_diff
