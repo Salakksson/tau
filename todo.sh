4d5e269ecabe1548fbafc25482bc2b00abb36f32
Alex Salek <alexsalek2007@gmail.com>
2025-05-18 23:10:41 +0100

    todo finder script

---
+++ todo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ALL=false
+
+if [ "$1" = "all" ] ;
+then
+	ALL=true
+fi
+
+FIND="todo"
+
+IGNORED="src/diag.c src/diag.h"
+
+SPECS="src/"
+
+for f in $IGNORED;
+do
+	spec=":!$f"
+	SPECS="$SPECS $spec"
+done
+
+output=$(git grep -nE --ignore-case $FIND -- $SPECS)
+
+lines=$(echo -e "$output" | wc -l)
+
+echo -e "Found $lines TODOs"
+
+if $ALL ;
+then
+	echo "$output"
+	exit 0
+fi
+index=$(( $RANDOM % $lines ))
+
+sed "${index}q;d" <(echo "$output")
